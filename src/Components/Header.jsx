d55b0eabf0024a49d5e1b387230909f16da4d766
adityajha1023 <adityajha.1023@gmail.com>
2026-01-30 03:12:23 +0530

    ad

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,14 +1,14 @@
-import { Share } from 'lucide-react'
+import { Ellipsis, Share } from 'lucide-react'
 import React from 'react'
 
 function Header() {
     return (
-        <div className='h-16 ml-[20%] flex justify-between items-center border-b border-gray-200 bg-gray-50 pl-4 pr-8 py-4 font-p'>
+        <div className='h-16 ml-[18%] flex justify-between items-center border-b border-gray-200 bg-gray-50 pl-4 pr-8 py-4 font-p'>
             <a href="/" className='font-semibold text-h6 flex items-center'>
-            <img src="./logo.png" alt="Nexus" className='h-12' />
             Nexus</a>
             <ul className='flex justify-center items-center gap-6 font-normal text-gray-500'>
-                <a href="#" className='hover:text-black transition-all duration-300 flex items-center gap-2'>Share <Share className="w-4 h-4" />  </a>
+                <a href="#" className='hover:text-black transition-all duration-300 flex items-center gap-2'>Share <Share className="w-4 h-4" /></a>
+                <a href="#" className='hover:text-black transition-all duration-300'><Ellipsis className="w-4 h-4" /></a>
             </ul>
         </div>
     )
--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import { Box, History, Newspaper, SearchIcon, SquarePen } from 'lucide-react'
+import user from '../assets/user.jpg'
 
 function Navigation() {
     return (
-        <div className="md:block hidden h-screen fixed bg-gray-50 border-r w-[20%] overflow-y-scroll scrollable-hide-scrollbar z-50">
-            asdf
+        <div className="md:block hidden h-screen fixed bg-gray-50 border-r w-[18%] overflow-y-scroll p-4 z-50">
+            <img src="./logo.png" alt="Nexus" className='h-12' />
+            <ul className='flex flex-col justify-center items-center gap-4'>
+            <a href="#" className='w-full flex justify-center items-center gap-2 border border-gray-500 text-gray-600 p-2 mt-4 rounded-full hover:text-white hover:bg-black transition-all duration-500'> 
+                <SquarePen className='w-4 h-4' />New Chat
+            </a>
+            <a href="#" className='w-full flex justify-start items-center gap-2 text-gray-500 px-2 pt-2 hover:text-black transition-all duration-300 hover:scale-[102%]'>
+                <SearchIcon className='w-4 h-4' />Search chats
+            </a>
+            <a href="#" className='w-full flex justify-start items-center gap-2 text-gray-500 px-2 hover:text-black transition-all duration-300 hover:scale-[102%]'>
+                <History className='w-4 h-4' />History
+            </a>
+            <a href="#" className='w-full flex justify-start items-center gap-2 text-gray-500 px-2 hover:text-black transition-all duration-300 hover:scale-[102%]'>
+                <Box className='w-4 h-4' />AI Models
+            </a>
+            <a href="#" className='w-full flex justify-start items-center gap-2 text-gray-500 px-2 hover:text-black transition-all duration-300 hover:scale-[102%]'>
+                <Newspaper className='w-4 h-4' />News
+            </a>
+            </ul>
+
+            <ul className='flex flex-col justify-center items-center'>
+               <img src={user} alt="" />
+            </ul>
         </div>
     )
 }
